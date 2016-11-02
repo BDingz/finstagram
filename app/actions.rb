5c7292397faf17f52e2d7405084e7e674a52325e
Becky Dingwell <dingwell.rebecca@gmail.com>
2016-11-02 23:17:31 +0000

    installed Sinatra, started working with Ruby

---
+++ app/actions.rb
@@ -0,0 +1,11 @@
+get '/' do
+    return username = "sharky_j"
+    avatar_url = "http://naserca.com/images/sharky_j.jpg"
+    photo_url = "http://naserca.com/images/shark.png"
+    time_ago_in_minutes = 15
+    like_count = 0
+    comment_count = 0
+    comments = [
+        "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
+        ]
+end
