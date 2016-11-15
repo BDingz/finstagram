abf1eb36bcc404d8e266d3fe887d3c2d31c3bce2
Becky Dingwell <dingwell.rebecca@gmail.com>
2016-11-15 01:26:54 +0000

    ActiveRecord

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base 
     
+    has_many :posts
+    has_many :comments
+    has_many :likes
+    
 end
+
+ #create_table "users", force: :cascade do |t|
+    #t.string   "username"
+    #t.string   "avatar_url"
+    #t.string   "email"
+    #t.string   "password"
+    #t.datetime "created_at"
+    #t.datetime "updated_at"
