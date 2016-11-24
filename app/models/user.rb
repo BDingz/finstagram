d7aafa702cc6091336dd30269d67a20b7897b758
Becky Dingwell <dingwell.rebecca@gmail.com>
2016-11-24 00:21:58 +0000

    RESTful Routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ActiveRecord::Base
     has_many :comments
     has_many :likes
     
-    validates_presence_of :email, :avatar_url, :username, :password
-    validates_uniqueness_of :email, :username
+    validates_presence_of :email, 
+                          :avatar_url, 
+                          :username, 
+                          :password
+                          
+    validates_uniqueness_of :email, 
+                            :username
     
 end
 
