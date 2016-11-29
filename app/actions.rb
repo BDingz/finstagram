fb5c138dda659496d3b7ea063cb544f109fb1dcc
Becky Dingwell <dingwell.rebecca@gmail.com>
2016-11-29 00:53:35 +0000

    likes

--- app/actions.rb
+++ app/actions.rb
@@ -87,14 +87,25 @@ post '/comments' do
     redirect (back)
 end 
 
-#get '/posts/:id' do 
-    #@post = Post.find(params[:id])
-    #erb(:"posts/show")
-#end 
+post '/likes' do
+    post_id = params[:post_id]
+    like = Like.new({ post_id: post_id, user_id: current_user
+.id})
+    like.save
+    redirect (back)
+end 
 
-#if comment.user != current_user
-	#halt(401, erb(:error_401))
-#end
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
+
+get '/posts/:id' do 
+    @post = Post.find(params[:id])
+    erb(:"/posts/show")
+end 
+
 
 
 
